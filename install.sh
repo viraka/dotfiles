9a1622ae62e32eadee37e1f3d33da09745f708bb
Viraj Prabhu <virajprabhup@gmail.com>
2026-06-20 15:20:18 +0530

    chore: link dotfiles/nvim to .config

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -sf ~/dotfiles/hypr ~/.config/
+ln -sf ~/dotfiles/waybar ~/.config/
+ln -sf ~/dotfiles/kitty ~/.config/
+ln -sf ~/dotfiles/rofi ~/.config/
+ln -sf ~/dotfiles/nvim ~/.config/
+
+echo "Dotfiles linked!"
